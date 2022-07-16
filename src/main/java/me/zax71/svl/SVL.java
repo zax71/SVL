ef1f7042a86d435e7097d8c889edac6876767193
Zax71 <zabax32@gmail.com>
2022-07-16 13:56:29 +0100

    Add bStats

--- src/main/java/me/zax71/svl/SVL.java
+++ src/main/java/me/zax71/svl/SVL.java
@@ -3,6 +3,7 @@ package me.zax71.svl;
 import co.aikar.commands.PaperCommandManager;
 import me.zax71.svl.commands.SVLCommand;
 import me.zax71.svl.events.vote;
+import org.bstats.bukkit.Metrics;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.configuration.file.FileConfiguration;
@@ -21,6 +22,10 @@ public final class SVL extends JavaPlugin {
         plugin = this;
         saveDefaultConfig();
         FileConfiguration config = getConfig();
+
+        // Bstats
+        int pluginId = 15800; // <-- Replace with the id of your plugin!
+        Metrics metrics = new Metrics(this, pluginId);
 
         // Register vote class
         this.getServer().getPluginManager().registerEvents(new vote(), this);
